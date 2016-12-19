bcc8d7f0132f9f00a480e6db86138568c9a95af5
Florian Schmickmann <flo.schmickmann@googlemail.com>
2016-12-19 23:30:50 +0100

    ...

---
+++ serial_passthrough/serial_passthrough.ino
@@ -0,0 +1,20 @@
+void setup() {
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+  
+  Serial.begin(115200);
+  while (!Serial) {}
+  
+  Serial1.begin(115200);
+}
+
+void loop() { // run over and over
+  if (Serial1.available()) {
+    digitalWrite(13, HIGH);
+    Serial.write(Serial1.read());
+    digitalWrite(13, LOW);
+  }
+  if (Serial.available()) {
+    Serial1.write(Serial.read());
+  }
+}
